03194ea6099361cfd46553628ec8bfe274418d73
yuvanshugargashya <yuvanshugargashya52@gmail.com>
2023-10-12 00:49:39 +0530

    git links

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -26,8 +26,8 @@ function Projects() {
               imgPath={chatify}
               isBlog={false}
               title="Real time chatting application"
-              description="a real time application to connect multiple users at a place to chat independently."
-              ghLink="https://github.com/smiley52/real-time-chatting-application"
+              description="a complete MERN real time application to connect multiple users at a place to chat independently."
+              ghLink="https://github.com/yuvanshugargashya/mern-chat-app.git"
               
             />
           </Col>
@@ -39,7 +39,7 @@ function Projects() {
               title="Tiny url"
               description="transforms long length given urls to short length by generating
               short unique id(mongoDb , node.js, express"
-              ghLink="https://github.com/smiley52/Tiny-url"
+              ghLink="https://github.com/yuvanshugargashya/url-shortner.git"
               
             />
           </Col>
@@ -51,7 +51,7 @@ function Projects() {
               title="Twitter bots detection"
               description="a machine learning bot classifier used for segregation of bots and
               humans using pandas and decision trees classifier"
-              ghLink="https://github.com/smiley52/twitterbotsdetection"
+              ghLink="https://github.com/yuvanshugargashya/twitter-bot-detection.git"
               
             />
           </Col>
@@ -64,7 +64,7 @@ function Projects() {
               description="generates random images as per discription given by the user
               (react.js , api)
               "
-              ghLink="https://github.com/smiley52/ai-images-generator"
+              ghLink="https://github.com/yuvanshugargashya/ai-images-generator.git"
               
             />
           </Col>
@@ -75,7 +75,7 @@ function Projects() {
               isBlog={false}
               title="ocr scanner"
               description="extracts text from img files"
-              ghLink="https://github.com/smiley52/ocr-scanner"
+              ghLink="https://github.com/yuvanshugargashya/ocr-scanner.git"
               // demoLink="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" <--------Please include a demo link here
             />
           </Col>
@@ -86,7 +86,7 @@ function Projects() {
               isBlog={false}
               title="Machine learning diabetes predictor"
               description="predicts weather person will have diabetes oe not on basis of some of his body reports using machine learning classifiers"
-              ghLink="https://github.com/smiley52/machine-learning-diabetes-predictor"
+              ghLink="https://github.com/yuvanshugargashya/machine_learning_diabetes-predictor.git"
               // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here 
             />
           </Col>
